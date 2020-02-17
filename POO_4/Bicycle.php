5f99c9d9c9b8dee859b94ab5b31681b5d9f304d4
nassilio <55001600+nassilio@users.noreply.github.com>
2020-02-17 14:05:28 +0100

    added missing return

--- POO_4/Bicycle.php
+++ POO_4/Bicycle.php
@@ -7,7 +7,7 @@ class Bicycle extends Vehicle implements LightableInterface
     public function switchOn(): bool
     {
         if ($this->currentSpeed >= 10){
-
+            return true;
         } else {
             return false;
         }
